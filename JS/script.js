6ca8e859ab4e4b6715c59aab342f9ca9902a93e8
MariamZead <mariam.a.zead@gmail.com>
2023-11-30 10:39:27 +0200

    Enabling mobile navigation toggle with JavaScript

--- JS/script.js
+++ JS/script.js
@@ -1,4 +1,16 @@
 ///////////////////////////////////////////////////////////
+///////////////////////////////////////////////////////////
+// Make mobile navigation work
+
+const btnNavEl = document.querySelector(".btn-mobile-nav");
+const headerEl = document.querySelector(".header");
+
+btnNavEl.addEventListener("click", function () {
+  headerEl.classList.toggle("nav-open");
+});
+
+//////
+///////////////////////////////////////////////////////////
 // Set current year
 const yearEl = document.querySelector(".year");
 const currentYear = new Date().getFullYear();
