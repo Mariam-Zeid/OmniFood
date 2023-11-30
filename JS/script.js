c558f0cd5c52d77a3fba9d0f2feff0503e3c9bdc
MariamZead <mariam.a.zead@gmail.com>
2023-11-30 11:31:47 +0200

    Adding sticky navbar using Javascript

--- JS/script.js
+++ JS/script.js
@@ -38,6 +38,33 @@ btnNavEl.addEventListener("click", function () {
 });
 
 ///////////////////////////////////////////////////////////
+// Sticky navigation
+
+const sectionHeroEl = document.querySelector(".hero-section");
+
+const obs = new IntersectionObserver(
+  function (entries) {
+    const ent = entries[0];
+    console.log(ent);
+
+    if (!ent.isIntersecting) {
+      document.body.classList.add("sticky");
+    }
+
+    if (ent.isIntersecting) {
+      document.body.classList.remove("sticky");
+    }
+  },
+  {
+    // In the viewport
+    root: null,
+    threshold: 0,
+    rootMargin: "-80px",
+  }
+);
+obs.observe(sectionHeroEl);
+
+///////////////////////////////////////////////////////////
 // Set current year
 
 const yearEl = document.querySelector(".year");
