09a87c783a7ea34940c804203523469c9da21853
MariamZead <mariam.a.zead@gmail.com>
2023-11-30 11:00:18 +0200

    Closing the mobile nav when clicking on any link

--- JS/script.js
+++ JS/script.js
@@ -20,6 +20,11 @@ allLinks.forEach(function (link) {
       const sectionEl = document.querySelector(href);
       sectionEl.scrollIntoView({ behavior: "smooth" });
     }
+
+    // Close mobile naviagtion
+    if (link.classList.contains("main-nav-link"))
+      headerEl.classList.toggle("nav-open");
+  });
 });
 
 ///////////////////////////////////////////////////////////
